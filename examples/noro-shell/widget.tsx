9b25678bae3d2667a9d528b9bb4c06f254f1b6d2
Dara Adedeji <daraadedeji07@gmail.com>
2026-07-23 23:30:18 -0700

    Center exact Noro icon coordinate boxes

--- examples/noro-shell/widget.tsx
+++ examples/noro-shell/widget.tsx
@@ -41,21 +41,21 @@ export default widget({
             onPress={noop}
             class="w-[100px] h-[100px] items-center justify-center rounded-[8.33px] rounded-bl-[37.5px] bg-[#1a1a1a] border border-[#0a0a0a] shadow-[0_1px_2px_0_#ffffff0d] shadow-inner pressed:bg-[#141414]"
           >
-            <icon d="M 7 7 L 9.333 7 L 9.333 21 L 7 21 Z M 11.083 14 L 21 21 L 21 7 Z" class="w-[28px] h-[28px] text-[#d0d0d0]" />
+            <icon d="M 7 7 L 9.333 7 L 9.333 21 L 7 21 Z M 11.083 14 L 21 21 L 21 7 Z" viewBox="0 0 28 28" class="w-[28px] h-[28px] text-[#d0d0d0]" />
           </button>
           <button
             onPress={() => setPlaying((value) => !value)}
             class="w-[100px] h-[100px] items-center justify-center rounded-[8.33px] bg-[#1a1a1a] border border-[#0a0a0a] shadow-[0_1px_2px_0_#ffffff0d] shadow-inner pressed:bg-[#141414]"
           >
             {playing
-              ? <icon d="M 7 5.833 L 11.667 5.833 L 11.667 22.166 L 7 22.166 Z M 16.333 5.833 L 21 5.833 L 21 22.166 L 16.333 22.166 Z" class="w-[28px] h-[28px] text-[#d0d0d0]" />
-              : <icon d="M 9.333 5.833 L 9.333 22.167 L 22.167 14 Z" class="w-[28px] h-[28px] text-[#d0d0d0]" />}
+              ? <icon d="M 7 5.833 L 11.667 5.833 L 11.667 22.166 L 7 22.166 Z M 16.333 5.833 L 21 5.833 L 21 22.166 L 16.333 22.166 Z" viewBox="0 0 28 28" class="w-[28px] h-[28px] text-[#d0d0d0]" />
+              : <icon d="M 9.333 5.833 L 9.333 22.167 L 22.167 14 Z" viewBox="0 0 28 28" class="w-[28px] h-[28px] text-[#d0d0d0]" />}
           </button>
           <button
             onPress={noop}
             class="w-[100px] h-[100px] items-center justify-center rounded-[8.33px] rounded-br-[37.5px] bg-[#1a1a1a] border border-[#0a0a0a] shadow-[0_1px_2px_0_#ffffff0d] shadow-inner pressed:bg-[#141414]"
           >
-            <icon d="M 7 21 L 16.917 14 L 7 7 Z M 18.667 7 L 21 7 L 21 21 L 18.667 21 Z" class="w-[28px] h-[28px] text-[#d0d0d0]" />
+            <icon d="M 7 21 L 16.917 14 L 7 7 Z M 18.667 7 L 21 7 L 21 21 L 18.667 21 Z" viewBox="0 0 28 28" class="w-[28px] h-[28px] text-[#d0d0d0]" />
           </button>
         </row>
       </column>
